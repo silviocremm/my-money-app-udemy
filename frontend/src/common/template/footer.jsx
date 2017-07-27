ad7cc4b24e25e49991f3d228bce87314d87920c4
silviocremm <silviocremm@gmail.com>
2017-07-27 08:32:08 -0300

    mudança no footer

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -4,7 +4,7 @@ export default props => (
     <footer className="main-footer">
         <strong>
             Copyright &copy; 2017
-            <a href="http://cod3r.com.br" target='_blank'> Cod3r</a>.
+            <a href="http://www.google.com.br" target='_blank'> Scremm</a>.
         </strong>
     </footer>
 )
