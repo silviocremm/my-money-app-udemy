54e9e2b8b2b641da5f8e2828673b2d818ab42039
silviocremm <silviocremm@gmail.com>
2017-08-07 08:31:00 -0300

    update aula (136)

--- frontend/src/billingCycle/billingCycleList.jsx
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -1,19 +1,38 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import { getList } from './billingCycleActions'
 
 class BillingCycleList extends Component {
+
+    componentWillMount() {
+        this.props.getList()       
+    }
+
+    renderRows() {
+        const list = this.props.list || []
+        return list.map(bc => (
+            <tr key={bc._id}>
+                <td>{bc.name}</td>
+                <td>{bc.month}</td>
+                <td>{bc.year}</td>
+            </tr>
+        ))
+    }
+
     render() {
         return (
             <div>
                 <table className="table">
-                    <thread>
+                    <thead>
                         <tr>
                             <th>Nome</th>
                             <th>Mês</th>
                             <th>Ano</th>
                         </tr>
-                    </thread>
+                    </thead>
                     <tbody>
-
+                        { this.renderRows() }
                     </tbody>
                 </table>
             </div>
@@ -21,4 +40,6 @@ class BillingCycleList extends Component {
     }
 }
 
-export default BillingCycleList
+const mapStateToProps = state => ({ list: state.billingCycle.list })
+const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList)
